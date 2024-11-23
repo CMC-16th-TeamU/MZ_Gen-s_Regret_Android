a45209be33c9089327dd1249ef03f9f861c5a9d4
Jim-swit <jim.jeon@swit.io>
2024-11-23 17:14:31 +0900

    feat/kakao 로그인 셋팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Kakao Login
+    implementation("com.kakao.sdk:v2-user:2.6.0")
+
 }
--- app/src/main/java/com/cmc/regret_aos/MainActivity.kt
+++ app/src/main/java/com/cmc/regret_aos/MainActivity.kt
@@ -2,10 +2,14 @@ package com.cmc.regret_aos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("KakaoTest", "Keyhash : ${Utility.getKeyHash(this)}")
     }
 }
